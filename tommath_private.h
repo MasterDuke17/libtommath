ccea7c54fb8596ae9dd3182555f7bb17658d6d88
Daniel Mendler <mail@daniel-mendler.de>
2019-08-05 12:29:10 +0200

    dont use private_mp_word

--- tommath_private.h
+++ tommath_private.h
@@ -159,7 +159,7 @@ typedef private_mp_word mp_word;
 #define MP_SIZEOF_BITS(type)    ((size_t)CHAR_BIT * sizeof(type))
 #define MP_MAXFAST              (int)(1uL << (MP_SIZEOF_BITS(mp_word) - (2u * (size_t)MP_DIGIT_BIT)))
 
-#define MP_WARRAY (int)(1uLL << (((CHAR_BIT * sizeof(private_mp_word)) - (2 * MP_DIGIT_BIT)) + 1))
+#define MP_WARRAY (int)(1uLL << (((CHAR_BIT * sizeof(mp_word)) - (2 * MP_DIGIT_BIT)) + 1))
 
 /* TODO: Remove PRIVATE_MP_PREC as soon as deprecated MP_PREC is removed from tommath.h */
 #ifdef PRIVATE_MP_PREC
